d5b4137901a6efa445a717e4b84cebfa89ddb32c
Caglar Polatoglu <polatoglucaglar@gmail.com>
2017-04-16 17:33:03 +0200

    Comparisons
    In this commit we generate a random number with random module. And we
    ask a guess from the user. Then we compare the random number and user's
    guess, if it is true we print "Correct!". If it is wrong we print
    "Wrong!" with the random number.

--- guess.py
+++ guess.py
@@ -1,5 +1,15 @@
-print("Hello, what is your favorite number?")
+import random
 
-number = input()
+minNumber = 1
+maxNumber = 100
+magicNumber = random.randint(minNumber, maxNumber)
 
-print("Your favorite number is: " + number)
+message = "Please guess a number between {0} and {1}"
+print(message.format(minNumber, maxNumber))
+
+guess = int(input())
+
+if guess == magicNumber:
+    print("Correct!")
+else:
+    print("Wrong! The magic number is: ", magicNumber)
